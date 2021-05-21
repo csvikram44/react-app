60db2eeb081ff0bc14a7b501e6eea9253e4940a6
Suraj Prajapat <suraj@magento360.com>
2021-05-21 19:53:42 +0530

    dev branch commit

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>suraj</p>
         <a
           className="App-link"
           href="https://reactjs.org"
